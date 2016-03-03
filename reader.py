33799e75a58f1555de1b60b209c38da27e8ef1b0
emily2zhou <emily2zhou@gmail.com>
2016-03-03 20:14:46 +0100

    updated reader and some changes to weather.nominal.arff

--- reader.py
+++ reader.py
@@ -7,26 +7,30 @@ def readARFF(filename):
     attributeList = []
     dataList = []
     isData = False
-
+    count = 0
     for line in file:
+        # print(count, line)
+        count += 1
         # If not a comment and not a new line
         if line[0] != '%' and line[0] != '\n':
             # If it has the relation tag
-            if line[1].lower() == 'r':
+            if line[0:2].lower() == '@r':
                 relationName = line[10:]
             # If it has the attribute tag
-            if line[1].lower() == 'a':
+            if line[0:2].lower() == '@a':
                 categories = []
-                tempList = line[11:].split("\t")
+                tempList = line[11:].split()
                 categories.append(tempList[0])
+                # print(tempList)
                 tempString = tempList[1].strip()[1:-1]
+                # print(tempString)
                 splitString = tempString.split(',')
                 for item in splitString:
                     categories.append(item)
                 attributeList.append(categories)
 
             # If it has the data tag
-            if line[1].lower() == 'd':
+            if line[0:2].lower() == '@d':
                 isData = True
 
             if isData and line[0] != '@':
