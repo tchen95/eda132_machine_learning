478f8ca1ffb2552c47091432dccf8cf372233d26
emily2zhou <emily2zhou@gmail.com>
2016-03-05 23:40:32 +0100

    new .arff file, multi-classification

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import id3
 import reader
 
-relationName, attributeList, dataList = reader.readARFF("restaurants.arff")
+relationName, attributeList, dataList = reader.readARFF("contact-lenses.arff")
 # tester = id3.tree("heyo",[4,5])
 # print(tester)
+
+print(relationName)
+print(attributeList)
+print(dataList)
+
 print(id3.id3(dataList,attributeList,dataList))
 
