68293eb41c7d98371a7963786ab72c80103a68f1
Tammy Chen <tchen95@berkeley.edu>
2016-03-05 23:41:34 +0100

    waiting for these merge conflicts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import id3
 import reader
 
-relationName, attributeList, dataList = reader.readARFF("restaurants.arff")
+relationName, attributeList, dataList = reader.readARFF("weather.nominal.arff")
 # tester = id3.tree("heyo",[4,5])
 # print(tester)
 print(id3.id3(dataList,attributeList,dataList))
